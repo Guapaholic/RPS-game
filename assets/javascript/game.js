9b73b78e2f18ec5d34f905fc0d96a6f6e3fe423b
Francis <juliancruz1892@gmail.com>
2018-07-02 16:33:54 -0400

    updated html and id

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var player1Choice = ""
+var player2Choice = ""
+var player1Wins = 0
+var player1Ties = 0
+var player1Loss = 0
+
+var database = firebase.database()
+
+if (player1Choice === player2Choice) {
+  ties++
+}
